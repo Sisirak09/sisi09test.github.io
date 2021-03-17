09575f8a7e8c8bc70f0d60cad6e8d0e8009bd499
RVC <kameleon_Reloaded@protonmail.com>
2021-03-17 04:28:41 -0600

    Big Update

--- 750/webkit-7.50/external/ps4nc.js
+++ 750/webkit-7.50/external/ps4nc.js
@@ -115,8 +115,10 @@ function setupRW() {
 	if(!read64(g_jsview_butterfly.sub(16)).equals(new Int64("0xffff000000001337")))
 		die("[!] Failed to setup addrof/fakeobj primitives");
 	debug_log("[+] Succesfully got addrof/fakeobj");
-	debug_log("[+] PS4 Jailbreak With Sucess!");
-	debug_log("[+] Waiting Payload on Port NetCat 9020!");
+	debug_log("[+] PS4 Exploit With Success!!");
+	debug_log("[+] Waiting For Payload/Netcat on Port 9020!!");
+
+	
 
 	/* Getting code execution */
 	/* ... */
