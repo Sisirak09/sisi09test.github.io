68dda0f812cd3248c0cab6040389dd4adfba1432
RVC <kameleon_Reloaded@protonmail.com>
2021-05-09 00:21:06 -0600

    PassCounter

--- 75XV3/Auto/jb/oldjb.js
+++ 75XV3/Auto/jb/oldjb.js
@@ -9,8 +9,10 @@ var _ = malloc_nogc.pop();
 var _ = malloc_nogc.pop();
 var _ = malloc_nogc.pop();
 if(main_ret == 179 || main_ret == 0){
+	localStorage.passcount = ++localStorage.passcount;window.passCounter.innerHTML=localStorage.passcount;
 	document.getElementById("progress").innerHTML="PS4 Jailbreak 7.5X Jailbreak Complete !!";
 } else{
+	localStorage.failcount = ++localStorage.failcount;window.failCounter.innerHTML=localStorage.failcount;
 	alert("Jailbreak Failed!! Please Restart and Start Fresh!!!");
 	document.getElementById("progress").innerHTML="PS4 Jailbreak 7.5X Jailbreak Failed, Restart and Start Fresh !!";
 } 
